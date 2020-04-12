28d4eef5a5e43bcbeb86d09d325dd099cca2b681
Beg <a.behaghel@inato.com>
2020-04-12 19:50:25 +0200

    ✨ Add request to the server from the client

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
+const App = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const getHelloWorld = async () => {
+      const response = await axios.get(
+        `http://${process.env.REACT_APP_SERVER_URL!}`,
+      );
+      setData(response.data);
+    };
+    getHelloWorld();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +23,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        {data ? data : "loading..."}
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -21,6 +35,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,7 +1,8 @@
 import * as Koa from "koa";
+import * as cors from "@koa/cors";
 
 const app = new Koa();
-
+app.use(cors({ origin: "*" }));
 app.use(async (ctx) => {
   ctx.body = "Hello World";
 });
