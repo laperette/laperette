b7f262c18e793365dd768057e8cbeca1f340c0c6
Beg <a.behaghel@inato.com>
2020-04-26 15:31:07 +0200

    💄 Use css-grid to display the calendar, and display the bookings

--- packages/client/src/Calendar/Calendar.tsx
+++ packages/client/src/Calendar/Calendar.tsx
@@ -5,8 +5,13 @@ import {
   getMonth,
   getYear,
   setMonth,
+  setDate,
   startOfMonth,
+  getDate,
+  isWithinInterval,
+  parseISO,
 } from "date-fns";
+import styled from "styled-components";
 
 const WEEK_DAYS_NAMES = [
   "Lundi",
@@ -62,8 +67,18 @@ export const useCalendarActions = ({ date }: { date: Date }) => {
     incrementMonth,
   };
 };
+export type Booking = {
+  readonly end_date: string;
+  readonly name: string;
+  readonly start_date: string;
+  readonly surname: string;
+};
 
-export const Calendar = () => {
+export const Calendar = ({
+  bookings,
+}: {
+  bookings: ReadonlyArray<Booking>;
+}) => {
   const today = new Date();
   const {
     currentMonthNumber,
@@ -74,49 +89,84 @@ export const Calendar = () => {
   const currentMonthName = MONTHS_NAMES[currentMonthNumber];
 
   const weekDays = WEEK_DAYS_NAMES.map((day) => {
-    return <th key={day}>{day}</th>;
+    return (
+      <WeekDay key={day} weekDay={day}>
+        {day}
+      </WeekDay>
+    );
   });
 
   const monthDate = setMonth(today, currentMonthNumber);
-  const firstDayOfMonth = startOfMonth(monthDate);
-  const blanks = [];
-  for (let i = 0; i < getDay(firstDayOfMonth) - 1; i++) {
-    blanks.push(<td>{""}</td>);
-  }
   const daysInMonth = [];
   for (let d = 1; d <= getDaysInMonth(monthDate); d++) {
-    daysInMonth.push(<td key={d}>{d}</td>);
+    const day = setDate(monthDate, d);
+    const booking = bookings.find((booking) =>
+      isWithinInterval(day, {
+        start: parseISO(booking.start_date),
+        end: parseISO(booking.end_date),
+      }),
+    );
+    daysInMonth.push(<Day key={d} day={day} booking={booking} />);
   }
-  const totalSlots = [...blanks, ...daysInMonth];
-  let rows: JSX.Element[][] = [];
-  let cells: JSX.Element[] = [];
-  totalSlots.forEach((row, i) => {
-    if (i % 7 !== 0) {
-      cells.push(row); // if index not equal 7 that means not go to next week
-    } else {
-      rows.push(cells); // when reach next week we contain all td in last week to rows
-      cells = []; // empty container
-      cells.push(row); // in current loop we still push current row to new container
-    }
-    if (i === totalSlots.length - 1) {
-      // when end loop we add remain date
-      rows.push(cells);
-    }
-  });
+
   return (
-    <table>
-      <thead>
+    <Container>
+      <Header>
         {currentYear}
         <button onClick={decrementMonth}>Previous</button>
         {currentMonthName}
         <button onClick={incrementMonth}>Next</button>
-        <tr>{weekDays}</tr>
-      </thead>
-      <tbody>
-        {rows.map((d, i) => (
-          <tr>{d}</tr>
-        ))}
-      </tbody>
-    </table>
+      </Header>
+      {weekDays}
+      {daysInMonth.map((d, i) => d)}
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  height: 100%;
+  display: grid;
+  grid-template-columns: repeat(7, auto);
+  grid-template-rows: 24px 24px repeat(6, auto);
+  grid-template-areas:
+    "header header header header header header header"
+    "Lundi Mardi Mercredi Jeudi Vendredi Samedi Dimanche"
+    "day-1 day-2 day-3 day-4 day-5 day-6 day-7"
+    "day-8 day-9 day-10 day-11 day-12 day-13 day-14"
+    "day-15 day-16 day-17 day-18 day-19 day-20 day-21"
+    "day-22 day-23 day-24 day-25 day-26 day-27 day-28"
+    "day-29 day-30 day-31 day-32 day-33 day-34 day-35"
+    "day-36 day-37 day-38 day-39 day-40 day-41 day-42";
+  column-gap: 8px;
+  row-gap: 8px;
+`;
+
+const WeekDay = styled.div<{ weekDay: string }>`
+  font-weight: bold;
+  align-self: center;
+  text-align: center;
+  grid-area: ${({ weekDay }) => weekDay};
+`;
+
+const StyledDay = styled.div<{ day: string }>`
+  grid-area: ${({ day }) => day};
+  border: 1px solid black;
+`;
+
+const Header = styled.div`
+  grid-area: header;
+  display: flex;
+  align-self: center;
+  justify-content: space-around;
+`;
+
+const Day = ({ day, booking }: { day: Date; booking?: Booking }) => {
+  const date = getDate(day);
+  const dayNumber = date + (getDay(startOfMonth(day)) || 7) - 1;
+  return (
+    <StyledDay day={`day-${dayNumber}`} key={date}>
+      {date}
+      {booking && booking.name}
+    </StyledDay>
   );
 };
