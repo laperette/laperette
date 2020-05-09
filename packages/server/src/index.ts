c0a3b334ebb9000258674eacdd167380b5abb206
Beg <a.behaghel@inato.com>
2020-05-09 15:28:29 +0200

    🐛 Make the server compile

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -9,7 +9,7 @@ app.use(cors({ origin: "*" }));
 app.use(bodyParser());
 app.use(router.routes());
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server started. Listening on port ${port}`);
 });
