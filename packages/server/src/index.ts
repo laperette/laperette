202b78315ce11d2bb479fd18e013f5c649dc5a95
Beg <a.behaghel@inato.com>
2020-08-16 22:08:18 +0200

    🔊 Logging errors

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -6,6 +6,16 @@ import { config } from "./config";
 
 const app = new Koa();
 
+app.use(async (ctx, next) => {
+  try {
+    await next();
+  } catch (err) {
+    ctx.status = err.status || 500;
+    ctx.body = err.message;
+    ctx.app.emit("error", err, ctx);
+  }
+});
+
 app.use(cors({ origin: config.corsOrigin, credentials: true }));
 app.use(bodyParser());
 app.use(router.routes());
@@ -13,4 +23,8 @@ app.use(router.routes());
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`Server started. Listening on port ${port}`);
+});
+
+app.on("error", (err) => {
+  console.log(err);
 });
