d77623366c210e36bd15eab6f4a05a63dc5609b0
Axel <mtn.axel@gmail.com>
2020-08-28 19:36:00 +0100

    Added seeding for accounts and bookings

--- packages/server/src/scripts/seed.ts
+++ packages/server/src/scripts/seed.ts
@@ -1,26 +1,88 @@
-// import { Booking } from "../domain";
-// import { addDays } from "date-fns";
-// import { knex } from "../db/db";
+import { addDays } from "date-fns";
+import { knex } from "../db/db";
+import { hashPassword } from "../utils/auth";
 
-// const serializeBooking = (booking: Booking) => ({
-//   end_date: booking.endDate,
-//   start_date: booking.startDate,
-//   first_name: booking.firstName,
-//   last_name: booking.lastName,
-// });
+const seedDatabase = async () => {
+  const createSeedingAccount = async (
+    firstName: string,
+    lastName: string,
+    email: string,
+    isMember: boolean,
+    isAdmin: boolean,
+  ): Promise<string> => {
+    const dumbPassword = await hashPassword("password");
+    const account = {
+      first_name: firstName,
+      last_name: lastName,
+      email,
+      is_member: isMember,
+      is_admin: isAdmin,
+      password: dumbPassword,
+    };
 
-// const seedDatabase = async () => {
-//   const today = new Date();
-//   const duration = 5;
-//   const booking = new Booking({
-//     endDate: addDays(today, duration),
-//     firstName: "Christophe",
-//     startDate: today,
-//     lastName: "Martin",
-//   });
-//   console.log("Inserting", booking);
-//   const result = await knex.table("bookings").insert(serializeBooking(booking));
-//   console.log("Inserted", result);
-// };
+    return knex.table("accounts").insert(account).returning("account_id");
+  };
 
-// seedDatabase();
+  const createSeedingBooking = async (
+    accountId: string,
+    randomDate: number,
+    stayLength: number,
+    status: string,
+  ): Promise<void> => {
+    const today = new Date();
+    const fakeBooking = {
+      booker_id: accountId,
+      arrival_time: addDays(today, randomDate),
+      departure_time: addDays(today, randomDate + stayLength),
+      comments: "Eager to be there!",
+      companions: ["Alain Gerbault", "Olivier de Kersauson"],
+      status,
+    };
+
+    return knex("bookings").insert(fakeBooking).returning("booking_id");
+  };
+
+  const clearTables = async () => {
+    Promise.all([knex("accounts").del(), knex("bookings").del()]);
+  };
+
+  console.log("Clearing tables");
+  await clearTables();
+  console.log("Cleared tables");
+
+  console.log("Inserting account");
+  const [adminId] = await createSeedingAccount(
+    "Eric",
+    "Tabraly",
+    "admin@gmail.com",
+    true,
+    true,
+  );
+  console.log("Inserted account");
+  console.log("Inserting account");
+  const [memberId] = await createSeedingAccount(
+    "Bernard",
+    "Moitessier",
+    "member@gmail.com",
+    true,
+    false,
+  );
+  console.log("Inserted account");
+
+  console.log("Inserting booking");
+  await createSeedingBooking(adminId, 3, 4, "approved");
+  console.log("Inserted booking");
+  console.log("Inserting booking");
+  await createSeedingBooking(memberId, 0, 2, "pending");
+  console.log("Inserted booking");
+  console.log("Inserting booking");
+  await createSeedingBooking(memberId, -10, 7, "rejected");
+  console.log("Inserted booking");
+  return;
+};
+
+try {
+  seedDatabase();
+} catch (error) {
+  console.log(error);
+}
