2aa7bdfcf3d7621b98e044540500cf8ac75c9e52
Axel <mtn.axel@gmail.com>
2020-08-31 21:27:55 +0100

    Cleaned wrong migration + Added migration to create houses table

---
+++ packages/server/src/db/migrations/20200829165048_create_house_table.ts
@@ -0,0 +1,33 @@
+import * as Knex from "knex";
+
+export async function up(knex: Knex): Promise<any> {
+  await knex.schema.createTable("houses", (table) => {
+    table.increments();
+    table.uuid("house_id").defaultTo(knex.raw("uuid_generate_v4()")).unique();
+    table.string("name");
+  });
+
+  await knex.schema.createTable("house_memberships", (table) => {
+    table.increments();
+    table.uuid("account_id").unique().notNullable();
+    table.foreign("account_id").references("account_id").inTable("accounts");
+    table.uuid("house_id").unique().notNullable();
+    table.foreign("house_id").references("house_id").inTable("houses");
+  });
+
+  await knex.schema.alterTable("bookings", (table) => {
+    table.uuid("house_id").unique().notNullable();
+    table.foreign("house_id").references("house_id").inTable("houses");
+  });
+}
+
+export async function down(knex: Knex): Promise<any> {
+  await knex.schema.alterTable("bookings", (table) => {
+    table.dropForeign(["house_id"]);
+    table.dropColumn("house_id");
+  });
+
+  await knex.schema.dropTableIfExists("house_memberships");
+
+  await knex.schema.dropTableIfExists("houses");
+}
